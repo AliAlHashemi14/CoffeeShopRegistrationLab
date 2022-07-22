02951b46c84868bc7d0b596b21426481a3691cbc
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-22 15:30:18 -0400

    first commit

---
+++ CoffeeShopRegistrationLab/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CoffeeShopRegistrationLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistrationLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            CoffeeMember cm = new CoffeeMember();
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult CoffeeSummary(CoffeeMember cm)
+        {
+            return View(cm);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistrationLab/Models/CoffeeMember.cs
@@ -0,0 +1,14 @@
+﻿namespace CoffeeShopRegistrationLab.Models
+{
+    public class CoffeeMember
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Vegan { get; set; }
+        public string FavDrink { get; set; }
+
+        //ASP Does not like constructors
+    }
+}
